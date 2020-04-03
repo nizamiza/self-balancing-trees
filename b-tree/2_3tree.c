d4f77523f17a34eab55e7799a3f119a92b0acaa4
Nizomiddin Toshpulatov <tremul27@gmail.com>
2020-04-03 04:02:59 +0200

    Implement insert logic: key insertion, node splitting and merging

--- b-tree/2_3tree.c
+++ b-tree/2_3tree.c
@@ -21,12 +21,23 @@
 	((c) <= (a)) ? __make_array(c, a, b) : __make_array(c, b, a)	\
 )
 
+#define __destruct_merge_args__(args)			\
+	struct _node *node 		= (args).node;		\
+	struct _node *left 		= (args).left;		\
+	struct _node *middle 	= (args).middle;	\
+	int prom_key 			= (args).prom_key;	\
+
 struct _node {
-	bool is_full;
+	bool isfull;
 	int low_key;
 	int high_key;
 	struct _node *left, *middle, *right;
 	struct _node *parent;
+};
+
+struct _merge_args {
+	struct _node *node, *left, *middle;
+	int prom_key;
 };
 
 static inline struct _node *_node(struct _node init)
@@ -39,21 +50,143 @@ static inline struct _node *_node(struct _node init)
 	return (*node = init, node);
 }
 
+static inline struct _node *_merge_with_parent(struct _merge_args args)
+{
+	__destruct_merge_args__(args);
+
+	if (!node->parent) {
+		
+		left->parent = middle->parent = node;
+
+		return _node((struct _node) {
+			.isfull 	= false,
+			.low_key 	= prom_key,
+			.left 		= left,
+			.middle 	= middle
+		});
+	}
+
+	if (!node->parent->isfull) {
+
+		if (node->parent->low_key < prom_key) {
+
+			node->parent->high_key = prom_key;
+			node->parent->middle = left;
+			node->parent->right = middle;
+
+		} else {
+
+			node->parent->high_key = node->parent->low_key;
+			node->parent->low_key = prom_key;
+
+			node->parent->left = left;
+			node->parent->right = node->parent->middle;
+			node->parent->middle = middle;
+		}
+
+		return (node->parent->isfull = true, node->parent);
+	}
+
+	int merge_prom_key;
+	struct _node *parent, *parent_left, *parent_middle;
+
+	if (prom_key <= node->parent->low_key) {
+
+		parent_left = _node((struct _node) {
+			.isfull 	= false,
+			.low_key 	= prom_key,
+			.left 		= left,
+			.middle 	= middle,
+			.parent 	= node->parent->parent
+		});
+
+		parent_middle = _node((struct _node) {
+			.isfull 	= false,
+			.low_key 	= node->parent->high_key,
+			.left 		= node->parent->middle,
+			.middle 	= node->parent->right,
+			.parent 	= node->parent->parent
+		});
+
+		merge_prom_key = node->parent->low_key;
+
+	} else if (prom_key >= node->parent->high_key) {
+
+		parent_left = _node((struct _node) {
+			.isfull 	= false,
+			.low_key 	= node->parent->low_key,
+			.left 		= node->parent->left,
+			.middle 	= node->parent->middle,
+			.parent 	= node->parent->parent
+		});
+
+		parent_middle = _node((struct _node) {
+			.isfull 	= false,
+			.low_key 	= prom_key,
+			.left 		= left,
+			.middle 	= middle,
+			.parent 	= node->parent->parent
+		});
+
+		merge_prom_key = node->parent->high_key;
+
+	} else {
+
+		parent_left = _node((struct _node) {
+			.isfull 	= false,
+			.low_key 	= node->parent->low_key,
+			.left 		= node->parent->left,
+			.middle 	= left,
+			.parent 	= node->parent->parent
+		});
+
+		parent_middle = _node((struct _node) {
+			.isfull 	= false,
+			.low_key 	= node->parent->high_key,
+			.left 		= middle,
+			.middle 	= node->parent->right,
+			.parent 	= node->parent->parent
+		});
+
+		merge_prom_key = prom_key;
+	}
+
+	return _merge_with_parent((struct _merge_args) {
+		.node 		= node->parent,
+		.left 		= parent_left,
+		.middle 	= parent_middle,
+		.prom_key 	= merge_prom_key
+	});
+}
+
 static inline struct _node *_split(struct _node *node, int key)
 {
 	int keys[] = __sort_keys(node->low_key, node->high_key, key);
+	struct _node *left, *middle;
 
-	 _node((struct _node) {
-		.left = _node((struct _node) { false, keys[0] }),
-		.middle = _node((struct _node) { false, keys[2] }),
-		.is_full = false,
-		.low_key = keys[1],
+	left = _node((struct _node) {
+		.isfull 	= false,
+		.low_key 	= keys[0],
+		.parent 	= node->parent
+	});
+
+	middle = _node((struct _node) {
+		.isfull 	= false,
+		.low_key 	= keys[2],
+		.parent 	= node->parent
+	});
+
+	return _merge_with_parent((struct _merge_args) {
+		.node 		= node,
+		.left 		= left,
+		.middle 	= middle,
+		.prom_key 	= keys[1]
 	});
 }
 
 static inline struct _node *_insert_key(struct _node *node, int key)
 {
-	if (node->is_full)
+	if (node->isfull)
 		return _split(node, key);
 
 	if (node->low_key < key) {
@@ -63,7 +196,7 @@ static inline struct _node *_insert_key(struct _node *node, int key)
 		node->low_key = key;
 	}
 	
-	return (node->is_full = true, node);
+	return (node->isfull = true, node);
 }
 
 static inline struct _node *_merge(struct _node *node, int key)
@@ -83,30 +216,31 @@ struct _node *search(struct _node *node, int key)
 	if (key <= node->low_key)
 		return search(node->left, key);
 
-	if (key > node->low_key && key < node->high_key)
-		return search(node->middle, key);
+	if (key >= node->high_key)
+		return search(node->right, key);
 
-	return search(node->right, key);
+	return search(node->middle, key);
 }
 
 struct _node *insert(struct _node *node, int key)
 {
-	if (!node)
-		return _node((struct _node) { false, key });
+	if (!node) {
+		return _node((struct _node) { 
+			.isfull = false,
+			.low_key = key 
+		});
+	}
 
 	if (!node->left)
 		return _insert_key(node, key);
 
 	if (key <= node->low_key)
-		node->left = insert(node->left, key);
+		return insert(node->left, key);
 
-	else if (key > node->low_key && key < node->high_key)
-		node->middle = insert(node->middle, key);
+	if (key > node->low_key && key < node->high_key)
+		return insert(node->middle, key);
 
-	else
-		node->right = insert(node->right, key);
-
-	return _merge(node, key);
+	return insert(node->right, key);
 }
 
 
