57375628bdca1a2b9bbcc4988d5f6e24e14483a3
Nizomiddin Toshpulatov <tremul27@gmail.com>
2020-04-09 23:40:29 +0200

    Add insertion and search execution time counter

--- treeio.c
+++ treeio.c
@@ -1,7 +1,20 @@
 #include <stdlib.h>
+#include <time.h>
 #include "include/treeio.h"
 #include "include/avltree.h"
 #include "include/avltree_assert.h"
+
+#define BILLION 1000000000.0
+#define __calc_delta_time(start, end) (	\
+	((end).tv_sec - (start).tv_sec) + 	\
+	((end).tv_nsec - (start).tv_nsec) 	\
+	/ BILLION							\
+)
+
+#define __time_d(t) (													\
+	printf(CLR_CYAN ">> %s: %lf seconds --\n" CLR_RESET, __func__, t)	\
+)
+
 
 static inline void exit_program(int code)
 {
@@ -12,19 +25,39 @@ static inline void exit_program(int code)
 static inline void insert_nodes(Node *root)
 {
 	int key;
+	double time_d;
+	
+	struct timespec start, end;
 
-	while (scanf("%d", &key) == 1)
+	while (scanf("%d", &key) == 1) {
+		time_d = 0.0;
+		clock_gettime(CLOCK_REALTIME, &start);
+		
 		*root = insert(*root, key);
+		clock_gettime(CLOCK_REALTIME, &end);
+		
+		time_d = __calc_delta_time(start, end);
+		__time_d(time_d);
+	}
 }
 
 static inline void search_node(Node root)
-{ int key;
+{ 
+	int key;
+	double time_d = 0.0;
+
+	struct timespec start, end;
 
 	if (scanf("%d", &key) != 1)
 		return;
 	getchar();
-
+	
+	clock_gettime(CLOCK_REALTIME, &start);
 	Node node = search(root, key);
+	
+	clock_gettime(CLOCK_REALTIME, &end);
+	time_d = __calc_delta_time(start, end);
+	__time_d(time_d);
 
 	if (node) {
 		print_node(node);
